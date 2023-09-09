d44f9d6f08b530ac86e4e25e58e3acbe167c1bc3
enna-ai <annelee3322@gmail.com>
2023-09-09 10:51:49 -0600

    added api/tasks.js

---
+++ common/src/api/tasks.js
@@ -0,0 +1,128 @@
+import axios from "axios";
+
+class TaskAPI {
+    constructor() {
+        this.api = axios.create({
+            baseURL: process.env.API_BASE_URL || "http://localhost:4000/api/v1/",
+        });
+    }
+
+    async makeRequest(method, url, data, headers) {
+        try {
+            const response = await this.api.request({
+                method,
+                url,
+                data,
+                headers,
+            });
+
+            if (response.status !== 200) {
+                throw new Error(`Request failed with status code ${response.status}`);
+            }
+
+            return response.data;
+        } catch (error) {
+            console.error(error.message);
+        }
+    }
+
+    async createTask(newTask, taskPriority, taskDueDate, token) {
+        const headers = {
+            Authorization: `Bearer ${token}`,
+        };
+
+        const data = {
+            task: newTask,
+            priority: taskPriority,
+            dueDate: taskDueDate,
+        };
+
+        return this.makeRequest("post", "tasks", data, null, headers);
+    }
+
+    async getTasks(token) {
+        const headers = {
+            Authorization: `Bearer ${token}`,
+        };
+
+        return this.makeRequest("get", "tasks", null, headers);
+    }
+
+    async getTask(taskId, token) {
+        const headers = {
+            Authorization: `Bearer ${token}`,
+        };
+
+        return this.makeRequest("get", `task/${taskId}`, null, headers);
+    }
+
+    async deleteTask(taskId, token) {
+        const headers = {
+            Authorization: `Bearer ${token}`,
+        };
+        
+        return this.makeRequest("delete", `task/${taskId}`, null, headers);
+    }
+
+    async updateTask(taskId, content, token) {
+        const headers = {
+            Authorization: `Bearer ${token}`,
+        };
+
+        const data = {
+            task: content,
+        };
+
+        return this.makeRequest("put", `task/${taskId}`, data, headers);
+    }
+
+    async completeTask(taskId, token) {
+        const headers = {
+            Authorization: `Bearer ${token}`,
+        };
+
+        return this.makeRequest("put", `tasks/${taskId}/complete`, null, headers);
+    }
+
+    async updateTaskDueDate(taskId, date, token) {
+        const headers = {
+            Authorization: `Bearer ${token}`,
+        };
+
+        const data = {
+            dueDate: date,
+        };
+
+        return this.makeRequest("put", `tasks/${taskId}/due`, data, headers);
+    }
+
+    async updateTaskPriority(taskId, taskPriority, token) {
+        const headers = {
+            Authorization: `Bearer ${token}`,
+        };
+
+        const data = {
+            priority: taskPriority,
+        };
+
+        return this.makeRequest("put", `tasks/${taskId}/priority`, data, headers);
+    }
+
+    async archiveTask(taskId, token) {
+        const headers = {
+            Authorization: `Bearer ${token}`,
+        };
+
+        return this.makeRequest("put", `task/${taskId}/archive`, null, headers);
+    }
+
+    async purgeTasks(token) {
+        const headers = {
+            Authorization: `Bearer ${token}`,
+        };
+
+        return this.makeRequest("delete", `tasks/purge`, null, headers);
+    }
+};
+
+export default new TaskAPI();
