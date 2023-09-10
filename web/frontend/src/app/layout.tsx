e639a5364f57c9bf75ca77d2a28a0246e7d44e15
enna-ai <annelee3322@gmail.com>
2023-09-09 20:07:38 -0600

    added 404 page, fix page structure for styling

--- web/frontend/src/app/layout.tsx
+++ web/frontend/src/app/layout.tsx
@@ -1,23 +1,24 @@
 import "@/styles/globals.scss";
-import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Header } from "@/components/Header";
 
 const inter = Inter({ subsets: ['latin'] });
 
-export const metadata: Metadata = {
+export const metadata = {
   title: "v busy 🐝",
   description: 'Task Manager',
 }
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
   return (
     <html lang="en">
       <body className={inter.className}>
         <Header />
-        <main>
-          {children}
-        </main>
+        {children}
       </body>
     </html>
   )
---
+++ web/frontend/src/pages/404.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const NotFound: React.FC = () => {
+    return (
+        <React.Fragment>
+            <h1>404 - Page Not Found</h1>
+            <p>Are you lost?</p>
+            <p><a href="/">$cd home</a></p>
+        </React.Fragment>
+    )
+};
+
+export default NotFound;
