b68d2e1b460856f665dfdbc29a95e50ecd8a61ad
enna-ai <annelee3322@gmail.com>
2023-09-27 16:34:14 -0600

    added styling to signup and login pages

--- web/frontend/src/app/login/page.tsx
+++ web/frontend/src/app/login/page.tsx
@@ -2,11 +2,13 @@
 
 import React from "react";
 import Link from "next/link";
+import Image from "next/image";
 import { toast, ToastContainer } from "react-toastify";
 import { Formik, Form, Field } from "formik";
 import * as Yup from "yup";
 import { useRouter } from "next/navigation";
 import axios from "axios";
+import BeeImage from "../../../assets/bee.png";
 
 interface FormValues {
     email: string;
@@ -47,24 +49,45 @@ const SignInPage: React.FC = () => {
 
     return (
         <>
-            <div>
-                <Formik
-                    initialValues={initialValues}
-                    onSubmit={handleLogin}
-                    validationSchema={Yup.object({
-                        email: Yup.string().required("Email address is required"),
-                        password: Yup.string().required("Password is required")
-                    })}
-                >
-                    <Form className="auth login">
-                        <Field type="email" id="email" name="email" placeholder="Email" />
-                        <Field type="password" id="password" name="password" placeholder="Password" />
-                        <button type="submit">Login</button>
-                        <ToastContainer />
-                    </Form>
-                </Formik>
+            <div className="loginPageContainer">
+                <div className="loginContainer">
+                    <div className="loginWrap">
+                        <div className="loginImage">
+                            <Image
+                                src={BeeImage}
+                                alt="bee image"
+                                width={250}
+                                height={250}
+                            />
+                        </div>
+                        <div className="loginFormContainer">
+                            <div className="loginForm">
+                                <h1>Login</h1>
+                                <p>Welcome to <span>Vbusy</span>! Please fill email and password to sign into your account.</p>
 
-                <p>Don&apos;t have an account? <Link href="/register">Sign Up</Link></p>
+                                <div className="formWrap">
+                                    <Formik
+                                        initialValues={initialValues}
+                                        onSubmit={handleLogin}
+                                        validationSchema={Yup.object({
+                                            email: Yup.string().required("Email address is required"),
+                                            password: Yup.string().required("Password is required")
+                                        })}
+                                    >
+                                        <Form className="formInput">
+                                            <Field type="email" id="email" name="email" placeholder="Email" />
+                                            <Field type="password" id="password" name="password" placeholder="Password" />
+                                            <button type="submit">Login</button>
+                                            <ToastContainer />
+                                        </Form>
+                                    </Formik>
+                                </div>
+
+                                <p className="loginText">Don&apos;t have an account? <Link href="/signup">Sign Up</Link></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </>
     )
--- web/frontend/src/app/signup/page.tsx
+++ web/frontend/src/app/signup/page.tsx
@@ -2,10 +2,13 @@
 
 import React from "react";
 import Link from "next/link";
+import Image from "next/image";
+import { toast, ToastContainer } from "react-toastify";
 import { Formik, Form, Field } from "formik";
 import * as Yup from "yup";
 import axios from "axios";
 import { useRouter } from "next/navigation";
+import BeeImage from "../../../assets/bee.png";
 
 interface FormValues {
     username: string;
@@ -28,31 +31,68 @@ const SignUpPage: React.FC = () => {
             console.log("Registered user!", response.data);
             localStorage.setItem("userInfo", JSON.stringify(response.data));
             router.push("/login");
-        } catch (error) {
-            console.error("Error registering user", error);
+        } catch (error: any) {
+            if (error.response && error.response.status === 401) {
+                toast.error(error.response.data.error, {
+                    position: "bottom-right",
+                    autoClose: 3000,
+                    hideProgressBar: true,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: "light",
+                });
+            } else {
+                console.error("Error during signup:", error.response);
+            }
         }
     };
-    
+
     return (
         <>
-            <Formik
-                initialValues={initialValues}
-                onSubmit={handleRegister}
-                validationSchema={Yup.object({
-                    username: Yup.string().required("Username is required"),
-                    email: Yup.string().required("Email address is required"),
-                    password: Yup.string().required("Password is required")
-                })}
-            >
-                <Form className="auth register">
-                    <Field type="text" id="username" name="username" placeholder="Username" minLength={2} maxLength={16} />
-                    <Field type="email" id="email" name="email" placeholder="Email" />
-                    <Field type="password" id="password" name="password" placeholder="Password" minLength={8} maxLength={24} />
-                    <button type="submit">Sign Up</button>
-                </Form>
-            </Formik>
+            <div className="loginPageContainer">
+                <div className="loginContainer">
+                    <div className="loginWrap">
+                        <div className="loginImage">
+                            <Image
+                                src={BeeImage}
+                                alt="bee image"
+                                width={250}
+                                height={250}
+                            />
+                        </div>
+                        <div className="loginFormContainer">
+                            <div className="loginForm">
+                                <h1>Sign Up</h1>
+                                <p>Welcome to <span>Vbusy</span>! Please fill email and password to sign into your account.</p>
 
-            <p>Already have an account? <Link href="/login">Login</Link></p>
+                                <div className="formWrap">
+                                    <Formik
+                                        initialValues={initialValues}
+                                        onSubmit={handleRegister}
+                                        validationSchema={Yup.object({
+                                            username: Yup.string().required("Username is required"),
+                                            email: Yup.string().required("Email address is required"),
+                                            password: Yup.string().required("Password is required")
+                                        })}
+                                    >
+                                        <Form className="formInput">
+                                            <Field type="text" id="username" name="username" placeholder="Username" minLength={2} maxLength={16} />
+                                            <Field type="email" id="email" name="email" placeholder="Email" />
+                                            <Field type="password" id="password" name="password" placeholder="Password" minLength={8} maxLength={24} />
+                                            <button type="submit">Sign Up</button>
+                                            <ToastContainer />
+                                        </Form>
+                                    </Formik>
+                                </div>
+
+                                <p className="loginText">Already have an account? <Link href="/login">Login</Link></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
